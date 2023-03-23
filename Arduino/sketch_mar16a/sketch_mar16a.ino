381f33f200a9f9a1fccac4de4ca4007908da06e4
Angel788 <angel-yahel@outlook.com>
2023-03-22 22:29:56 -0600

    Encendido de LCD y funciones basicas

--- Arduino/sketch_mar16a/sketch_mar16a.ino
+++ Arduino/sketch_mar16a/sketch_mar16a.ino
@@ -1,11 +1,8 @@
-#include <LiquidCrystal.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-int piso[4][16];
-int mario[2];
-int enemigos[4][16];
-
-byte customChar[][]={
-   {
+LiquidCrystal_I2C lcd(0x27,20,4);
+byte nubes[]{
     B00000,
     B00000,
     B00000,
@@ -13,9 +10,9 @@ byte customChar[][]={
     B11110,
     B01111,
     B11110,
-    B01010
-   },#NUBES
-   {
+    B01010  
+};
+byte enemigos[]{
     B00000,
     B00000,
     B00000,
@@ -23,9 +20,9 @@ byte customChar[][]={
     B00100,
     B01110,
     B11111,
-    B01010     
-   },#Enemigos
-   {
+    B01010  
+};
+byte mario[]{
     B01110,
     B01110,
     B01110,
@@ -33,16 +30,52 @@ byte customChar[][]={
     B01110,
     B10101,
     B00100,
-    B11011
-   }#Mario         
+    B11011  
 };
-
+byte tuberias[]{
+     B00000,
+     B00000,
+     B00000,
+     B11111,
+     B11111,
+     B01110,
+     B01110,
+     B01110  
+};
+byte piso[]{
+  	0B00000,
+	  0B00000,
+	  0B00000,
+	  0B00000,
+	  0B00000,
+	  0B00000,
+	  0B11111,
+	  0B11111
+};
 void setup() {
-  // put your setup code here, to run once:
-
+  lcd.init();
+  lcd.backlight();  
+  lcd.setCursor(0, 0);
+  lcd.createChar(0, nubes);
+  lcd.createChar(1, enemigos);
+  lcd.createChar(2, mario);
+  lcd.createChar(3, tuberias);
+  lcd.createChar(4, piso);   
+  //lcd.createChar(0,nubes);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcd.setCursor(0,0);
+  lcd.write(0);
+  lcd.setCursor(1,0);
+  lcd.write(1);
+  lcd.setCursor(2,0);
+  lcd.write(2);
+  lcd.setCursor(3,0);
+  lcd.write(3);
+  lcd.setCursor(4,0);
+  lcd.write(4);
+  lcd.setCursor(0, 1);
+  lcd.print("Mario");
+  lcd.setCursor(0,0);
 }
